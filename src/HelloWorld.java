e29cf466790f5bf36857b24906bb5bfdbd3adc48
onayanar <pramath505@gmail.com>
2016-01-08 22:54:45 -0500

    Update HelloWorld.java
    updated trace

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
  public static void main(String args[]) {
-  System.out.println("Entering Main");
+  System.out.println("Entering Main method");
   System.out.println("Hello World");
  }
 
